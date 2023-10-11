618356252b67bd2c9cf985e3ab8bb22d9e602167
root <root@Inspiron7460>
2023-10-10 21:50:47 -0300

    Atualizacao: projeto 10/10_001

---
+++ src/main/java/com/unopar/prgweb7/programacao_web/ProgramacaoWebApplication.java
@@ -0,0 +1,13 @@
+package com.unopar.prgweb7.programacao_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacaoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacaoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unopar/prgweb7/programacao_web/ProgramacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unopar.prgweb7.programacao_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
