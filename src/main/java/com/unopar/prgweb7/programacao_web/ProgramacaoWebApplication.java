b2f670d234f83e2a892c2498160892d8afeb8622
root <root@Inspiron7460>
2023-10-12 18:29:38 -0300

    Atualizacao: projeto 12/10_006

--- src/main/java/com/unopar/prgweb7/programacao_web/ProgramacaoWebApplication.java
+++ src/main/java/com/unopar/prgweb7/programacao_web/ProgramacaoWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "config")
 public class ProgramacaoWebApplication {
 
 	public static void main(String[] args) {
