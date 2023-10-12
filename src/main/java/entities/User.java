d8dfd88d65d2442b42ae96aa1b24e75d4d4e5495
root <root@Inspiron7460>
2023-10-12 16:12:06 -0300

    Atualizacao: projeto 12/10_003

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,4 +1,86 @@
 package entities;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String nome, String email, String telefone, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id != null ? id.equals(user.id) : user.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
--- src/main/java/resource/UserResource.java
+++ src/main/java/resource/UserResource.java
@@ -1,4 +1,15 @@
 package resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String list() {
+        return "Aqui irá listar os usuários";
+    }
 }
